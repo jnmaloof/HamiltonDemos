4a7a706c65e47cf429375ce5e3081a8a6253b108
Julin Maloof <jnmaloof@ucdavis.edu>
2013-07-23 22:53:10 -0700

    Initial commit to add files for binomial genetic drift simulator

---
+++ BinomialDrift/server.R
@@ -0,0 +1,17 @@
+# Server side of shiny app to plot binomial genetic drift simulations
+
+library(shiny)
+library(ggplot2)
+
+shinyServer(func=function(input,output){
+  
+  output$driftPlot <- renderPlot({
+      size = input$size*2 #assume individuals are diploid but there are 2n chromosomes
+      results <- matrix(nrow=input$generations+1,ncol=input$pops) #create matrix to hold results
+      results[1,] <- input$freq               #populate first row with initial frequencies
+      for (g in 2:(input$generations+1)) {    #loop through the generations
+        results[g,] <- rbinom(input$pops,size,prob=results[g-1,])/size #calculate the next generations frequencies
+      }
+      print( matplot(results,type="l",lwd=2,main=paste("n = ",size,", generations = ",input$generations),ylab = " frequency", ylim = c(0,1)))
+})
+})
---
+++ BinomialDrift/ui.R
@@ -0,0 +1,28 @@
+## Shiny App to illustrate genetic drift using a binomial model
+## Julin Maloof
+## July 23, 2013
+
+library(shiny)
+
+# Define UI 
+shinyUI(pageWithSidebar(
+  
+  # Application title
+  headerPanel("Genetic Drift--Bionomial Model"),
+  
+  # Sidebar with a slider input for number of observations
+  sidebarPanel(
+    numericInput("pops","Number of populations to simulate",value=10,min=1,max=100),
+    
+    numericInput("size", "Number of individuals in each population",value=100,min=2,max=100000),
+    
+    numericInput("generations","Number of generations to run simulation for",value=50,min=10,max=1000),
+    
+    numericInput("freq","Frequency of first allele",value=.5,min=0,max=1)
+  ),
+  
+  # Show a plot of the HWE and the user input population
+  mainPanel(
+    plotOutput("driftPlot")
+  )
+))
