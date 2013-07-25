1918445984ce5466346243d13da223daeddc3bf4
Julin Maloof <jnmaloof@ucdavis.edu>
2013-07-25 14:11:48 -0700

    Bug fixes Added inputs to change angle of view

--- MarkovDrift/server.R
+++ MarkovDrift/server.R
@@ -7,13 +7,15 @@ library(shiny)
 shinyServer(function(input,output,session) {
   
   observe({ #make sure that allele count is not greater than the total number of possible alleles
-    if (input$init.alleles > 2*input$size) {
-      updateSliderInput(session,"init.alleles",value=2*input$size)
+    print("observe")
+    if (input$initAlleles > 2*input$size) {
+      print("over")
+      updateSliderInput(session,"initAlleles",value=2*input$size)
     }
   })
   
-    results <- reactive({
-      size <- 2*input$size
+  results <- reactive({
+    size <- 2*input$size
     m.binom <- matrix(0,nrow=size+1,ncol=size+1,dimnames=list(from=0:size,to=0:size)) #this will hold the binomial transition probabilities
     
     #now we should be able to fill in the matrix by mutliplying the allele probabilities in the current generation by the binomial probabilities
@@ -27,18 +29,18 @@ shinyServer(function(input,output,session) {
     
     results <- matrix(0,ncol=size+1,nrow=input$generations,dimnames=list(generation=1:input$generations,allele.freq=0:size))
     
-    results[1,as.character(input$init.alleles)] <- 1
+    results[1,as.character(input$initAlleles)] <- 1
     
     for (g in 2:input$generations) {
       results[g,] <- results[g-1,] %*% m.binom
     }
     results
-    }
-    )
-    
+  }
+  )
+  
   output$resultsPlot <- renderPlot({
-    print(persp(results(),theta=70,phi=10,xlab="generation",ylab="allele count",zlab="proportion",col="skyblue"))
-  })
+    print(persp(results(),theta=input$theta,phi=input$phi,xlab="generation",ylab="allele count",zlab="proportion",col="skyblue"))
+  },height=600,width=600)
   
   output$resultsTable <- renderTable({
     round(results(),3)
--- MarkovDrift/ui.R
+++ MarkovDrift/ui.R
@@ -7,16 +7,20 @@ shinyUI(pageWithSidebar(
   headerPanel("Markov simulation of genetic drift"),
   
   sidebarPanel(
-    sliderInput("size","Number of individuals in population",value=10,min=1,max=1000),
+    sliderInput("size","Number of individuals in population",value=10,min=1,max=100),
     
-    sliderInput("init.alleles","Number of A alleles in first generation",value=5,min=0,max=2000),
+    sliderInput("initAlleles","Number of A alleles in first generation",value=5,min=0,max=200),
     
-    sliderInput("generations","Generations to simulate",value=5,min=2,max=100)
+    sliderInput("generations","Generations to simulate",value=5,min=2,max=50),
+    
+    sliderInput("theta","left-right viewing angle",value=80,min=0,max=360),
+    
+    sliderInput("phi","top-bottom viewing angle",value=10,min=-90,max=90)
   ),
   
   mainPanel(
     tabsetPanel(
-      tabPanel("Plot", plotOutput("resultsPlot")),
+      tabPanel("Plot", plotOutput("resultsPlot",height="100%")),
       tabPanel("Table", tableOutput("resultsTable"))
     )  )
 ))
